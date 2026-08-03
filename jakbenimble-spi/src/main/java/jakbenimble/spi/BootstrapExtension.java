423213b0b2bb3db418abaef1b68da7982b4a446e
Jeff Rogers <redbeardcodes@gmail.com>
2026-08-03 00:12:17 -0400

    Add name metadata to BootstrapExtension.

--- jakbenimble-spi/src/main/java/jakbenimble/spi/BootstrapExtension.java
+++ jakbenimble-spi/src/main/java/jakbenimble/spi/BootstrapExtension.java
@@ -3,6 +3,10 @@ package jakbenimble.spi;
 import jakarta.enterprise.inject.spi.BeanManager;
 
 public interface BootstrapExtension {
+	default String name() {
+		return getClass().getSimpleName();
+	}
+
 	public void configure(BeanManager bm);
 
 	public void destroy();
